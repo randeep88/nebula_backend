4f2cba1e9e5df8e35b8d53be82187b51bc0dbfa7
Randeep Singh <randeeprajpal9@gmail.com>
2026-02-22 19:03:38 +0530

    oiusgdghrtwkje

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,6 +6,9 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: "randeeprajpal9@gmail.com",
     pass: "wbnb wqha zoca cuwa",
